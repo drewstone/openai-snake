7a30ecd305658312171d6563550628a0365bc430
Drew Stone <drewstone329@gmail.com>
2020-03-22 19:50:35 -0600

    More updates

--- openai/lstm.py
+++ openai/lstm.py
@@ -15,6 +15,7 @@ def gen_sequences(num_sequences, max_bits=50):
     labels = []
     for i in range(num_sequences):
         inp = get_input(max_bits)
+        # parity = torch.tensor([1, 0]) if np.sum(inp) % 2 != 0 else torch.tensor([0, 1])
         parity = 1 if np.sum(inp) % 2 != 0 else 0
         inputs.append(inp)
         labels.append(parity)
@@ -36,44 +37,36 @@ class ParityDataset(Dataset):
 class ParityModel(nn.Module):
     def __init__(self, input_dim=1, hidden_dim=2, output_dim=1):
         super(ParityModel, self).__init__()
-        print('New LSTM: (input: {}, hidden: {}, output: {}\n'
-            .format(input_dim, output_dim, hidden_dim))
+        print('New LSTM: (input: {}, hidden: {}, output: {})\n'
+            .format(input_dim, hidden_dim, output_dim))
         self.lstm = nn.LSTM(input_dim, hidden_dim)
         self.linear_out = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
         x = x.view(-1, 1, 1)
         lstm_out    = self.run_through_lstm(x)
+        out         = F.relu(lstm_out)
         # print('LSTM layer output size: {}'.format(lstm_out.size()))
-        out         = self.linear_out(lstm_out)
+        out         = self.linear_out(out)
         # print('Linear label output size: {}'.format(out.size()))
-        out         = F.log_softmax(out.view(-1, 1), dim=1)
-        # print(out.size())
         return out
 
     def run_through_lstm(self, x):
-        # initialize the hidden state.
-        # hidden = (torch.randn(1, 1, 2),
-        #           torch.randn(1, 1, 2))
-        # for i in x:
-        #     out, hidden = self.lstm(i.view(-1, 1, 1), hidden)
-        print(x.size())
         out, h = self.lstm(x)
-        print(out.size())
-        # print(out, hidden)
-        return out
+        return out[-1]
 
 batch_size = 1
 num_sequences = 10000
-seq_len = 50
+seq_len = 10
 train_dataloader = DataLoader(ParityDataset(num_sequences, seq_len), batch_size)
 
-model = ParityModel(input_dim=1, hidden_dim=2, output_dim=1)
+model = ParityModel(input_dim=1, hidden_dim=10, output_dim=1)
 
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
 # loss_function = torch.nn.MSELoss()
-loss_function = torch.nn.BCELoss()
+loss_function = torch.nn.BCEWithLogitsLoss()
 # loss_function = torch.nn.NLLLoss()
+# loss_function = torch.nn.CrossEntropyLoss()
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 running_loss = 0.0
@@ -84,8 +77,8 @@ for inx, data in enumerate(train_dataloader):
     optimizer.zero_grad()
 
     out = model(inputs)
-    # print(out.size())
-    loss = loss_function(out, labels.float())
+    # print(out, labels)
+    loss = loss_function(out, labels.view(1, -1))
     loss.backward()
     optimizer.step()
 
@@ -94,4 +87,15 @@ for inx, data in enumerate(train_dataloader):
         # ...log the running loss
         print('training loss', running_loss / 1000)
         running_loss = 0.0
-    
+
+
+test_num_seq = 100
+test_dataloader = DataLoader(ParityDataset(test_num_seq, seq_len), batch_size)
+with torch.no_grad():
+    model.eval()
+    for inx, data in enumerate(train_dataloader):
+        inputs, labels = data
+        labels = labels.float().to(device=device)
+        out = model(inputs)
+        print(out, labels)
+
