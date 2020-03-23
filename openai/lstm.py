49afa33fa5bd46e9f487ddc8bc5b2b019a7ae120
Drew Stone <drewstone329@gmail.com>
2020-03-22 18:56:41 -0600

    Upds

--- openai/lstm.py
+++ openai/lstm.py
@@ -7,44 +7,8 @@ from torch.utils.data import DataLoader, Dataset
 
 torch.manual_seed(1)
 
-class ParityDataset(Dataset):
-    def __init__(self, num_sequences, seq_len):
-        self.datas, self.labels = gen_sequences(num_sequences, seq_len)
-
-    def __getitem__(self, index):
-        data, target = self.datas[index], self.labels[index]
-        return torch.tensor(data).float(), torch.tensor(target).float()
-
-    def __len__(self):
-        return len(self.datas)
-
-class ParityModel(nn.Module):
-    def __init__(self, input_dim=1, hidden_dim=2, output_dim=1):
-        super(ParityModel, self).__init__()
-        print('New LSTM: (input: {}, hidden: {}, output: {}\n'
-            .format(input_dim, output_dim, hidden_dim))
-        self.lstm = nn.LSTM(input_dim, hidden_dim)
-        self.linear_out = nn.Linear(hidden_dim, output_dim)
-
-    def forward(self, x):
-        lstm_out    = self.run_through_lstm(x)
-        # print('LSTM layer output size: {}'.format(lstm_out.size()))
-        out         = self.linear_out(lstm_out)
-        # print('Linear label output size: {}'.format(out.size()))
-        out         = F.log_softmax(out.view(-1, 1), dim=1)
-        # print(out)
-        return out
-
-    def run_through_lstm(self, x):
-        out, h = self.lstm(x.view(1, 1, -1))
-        return out
-
-
-
-# inputs = [torch.randn(1, 3) for _ in range(5)]  # make a sequence of length 5
-def get_input(max_bits):
+def get_input(max_bits=50):
     return [ np.random.randint(2) for _ in range(max_bits) ]
-
 
 def gen_sequences(num_sequences, max_bits=50):
     inputs = []
@@ -56,12 +20,56 @@ def gen_sequences(num_sequences, max_bits=50):
         labels.append(parity)
     return inputs, labels
 
+
+class ParityDataset(Dataset):
+    def __init__(self, num_sequences, seq_len):
+        self.data, self.labels = gen_sequences(num_sequences, seq_len)
+
+    def __getitem__(self, index):
+        datum, label = self.data[index], self.labels[index]
+        return torch.tensor(datum).float(), torch.tensor(label).float()
+
+    def __len__(self):
+        return len(self.data)
+
+
+class ParityModel(nn.Module):
+    def __init__(self, input_dim=1, hidden_dim=2, output_dim=1):
+        super(ParityModel, self).__init__()
+        print('New LSTM: (input: {}, hidden: {}, output: {}\n'
+            .format(input_dim, output_dim, hidden_dim))
+        self.lstm = nn.LSTM(input_dim, hidden_dim)
+        self.linear_out = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        x = x.view(-1, 1, 1)
+        lstm_out    = self.run_through_lstm(x)
+        # print('LSTM layer output size: {}'.format(lstm_out.size()))
+        out         = self.linear_out(lstm_out)
+        # print('Linear label output size: {}'.format(out.size()))
+        out         = F.log_softmax(out.view(-1, 1), dim=1)
+        # print(out.size())
+        return out
+
+    def run_through_lstm(self, x):
+        # initialize the hidden state.
+        # hidden = (torch.randn(1, 1, 2),
+        #           torch.randn(1, 1, 2))
+        # for i in x:
+        #     out, hidden = self.lstm(i.view(-1, 1, 1), hidden)
+        print(x.size())
+        out, h = self.lstm(x)
+        print(out.size())
+        # print(out, hidden)
+        return out
+
 batch_size = 1
 num_sequences = 10000
 seq_len = 50
 train_dataloader = DataLoader(ParityDataset(num_sequences, seq_len), batch_size)
 
-model = ParityModel(input_dim=seq_len, hidden_dim=2, output_dim=1)
+model = ParityModel(input_dim=1, hidden_dim=2, output_dim=1)
+
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0)
 # loss_function = torch.nn.MSELoss()
 loss_function = torch.nn.BCELoss()
@@ -76,6 +84,7 @@ for inx, data in enumerate(train_dataloader):
     optimizer.zero_grad()
 
     out = model(inputs)
+    # print(out.size())
     loss = loss_function(out, labels.float())
     loss.backward()
     optimizer.step()
